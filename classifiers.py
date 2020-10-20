4dcf67b8d5cbb9dcae87677485e792d5af6352c5
Osvaldo Fonseca <osvaldo@osvaldo>
2020-10-20 01:23:26 -0300

    update

--- classifiers.py
+++ classifiers.py
@@ -1,13 +1,16 @@
 import numpy as np
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import cross_val_score, cross_validate
+from sklearn.model_selection import cross_val_score, cross_validate, cross_val_predict
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.metrics import recall_score, precision_score,\
-        accuracy_score, f1_score
+        accuracy_score, f1_score, confusion_matrix, precision_recall_fscore_support
+from sklearn.decomposition import PCA
+from sklearn.naive_bayes import GaussianNB
+
 
 def readDatabase(input_file):
 
@@ -34,7 +37,7 @@ def getCompleteData(header, records_list):
 
     return (case_data, case_target)
 
-def getDataFromBestKFeaturesSelection(header, records_list, kv=20):
+def getDataFromBestKFeaturesSelection(header, records_list, kv=20, flag_chi2=False):
 
     case_data = []
     case_target = []
@@ -45,10 +48,30 @@ def getDataFromBestKFeaturesSelection(header, records_list, kv=20):
         case_data.append(data)
         case_target.append(target)
 
-    new_case_data = SelectKBest(k=kv).fit_transform(case_data,\
-            case_target)
+    if flag_chi2:
+        new_case_data = SelectKBest(chi2, k=kv).fit_transform(case_data,\
+                case_target)
+    else: 
+        new_case_data = SelectKBest(k=kv).fit_transform(case_data,\
+               case_target)
 
     return (new_case_data, case_target)
+
+def getDataPCATransformed(header, records_list, number_components):
+
+    case_data = []
+    case_target = []
+    for record in records_list:
+        data = record[:-1]
+        data = [float(x) for x in data]
+        target = record[-1]
+        case_data.append(data)
+        case_target.append(target)
+
+    pca = PCA(n_components=number_components)
+    pca.fit_transform(case_data, case_target)
+
+    return (case_data, case_target)
 
 def informScoresInfo(scores):
 
@@ -59,7 +82,7 @@ def informScoresInfo(scores):
     print("accuracy,precision,recall,f1_macro")
     print("%.2f\t%.2f\t%.2f\t%.2f"%(acc,precision,recall,f1_macro))
 
-def runRandomForest(case_data, case_target, max_depth_value):
+def runRandomForest(case_data, case_target, max_depth_value, conf_mat_flag=False):
 
     scoring_metrics = ["accuracy", "recall_macro", "f1_macro", "precision_macro"]
     clf = RandomForestClassifier(n_estimators=1000, max_depth=max_depth_value)
@@ -68,16 +91,41 @@ def runRandomForest(case_data, case_target, max_depth_value):
     
     informScoresInfo(scores)
 
+    if conf_mat_flag:
+        y_pred = cross_val_predict(clf, case_data, case_target, cv=10)
+        conf_mat = confusion_matrix(case_target, y_pred)
+        print(conf_mat)
 
-def runSVM(case_data, case_target, kernel_value, c_value):
+
+def runSVM(case_data, case_target, kernel_value, c_value, conf_mat_flag=False):
 
     scoring_metrics = ["accuracy", "recall_macro", "f1_macro", "precision_macro"]
     clf = SVC(kernel=kernel_value, C=c_value, cache_size=1000)
-    #clf = make_pipeline(StandardScaler(), SVC(kernel=kernel_value, C=c_value, cache_size=1000))
     scores = cross_validate(clf, case_data, case_target, cv=10,\
             scoring=scoring_metrics)
 
     informScoresInfo(scores)
+
+    if conf_mat_flag:
+        y_pred = cross_val_predict(clf, case_data, case_target, cv=10)
+        conf_mat = confusion_matrix(case_target, y_pred)
+        print(conf_mat)
+
+def runNaiveBayes(case_data, case_target, conf_mat_flag=False):
+
+    scoring_metrics = ["accuracy", "recall_macro", "f1_macro", "precision_macro"]
+    clf = GaussianNB()
+    scores = cross_validate(clf, case_data, case_target, cv=10,\
+            scoring=scoring_metrics)
+
+    informScoresInfo(scores)
+
+    if conf_mat_flag:
+        y_pred = cross_val_predict(clf, case_data, case_target, cv=10)
+        conf_mat = confusion_matrix(case_target, y_pred)
+        print(conf_mat)
+
+
 
 def runDecisionTree(header, records_list):
 
@@ -108,15 +156,34 @@ def selectBestFeatures(header, records_list):
     clf = DecisionTreeClassifier()
     print(np.mean(cross_val_score(clf, new_case_data, case_target, cv=100)))
 
+
+
 if __name__ == "__main__":
-    input_file = "data/callcenter_case_fixed.csv"
+    input_file = "data/callcenter_case_fixed_normalized.csv"
     header, records_list = readDatabase(input_file)
-    case_data, case_target = getCompleteData(header, records_list)
+    #case_data, case_target = getCompleteData(header, records_list)
+    case_data, case_target = getDataFromBestKFeaturesSelection(header, records_list, 5, True)
     #runDecisionTree(header, records_list)
-    c_values = [0.1, 1, 10, 100]
-    for c_value in c_values:
-        print("Using C: ", c_value)
-        runSVM(case_data, case_target, 'sigmoid', c_value)   
+    # c_values = [0.1, 1, 10, 100]
+    # for c_value in c_values:
+    #     print("Using C: ", c_value)
+    #     runSVM(case_data, case_target, 'sigmoid', c_value)
+    
+    # runSVM(case_data, case_target, 'linear', 1)   
     #selectBestFeatures(header, records_list)
-    #runRandomForest(header, records_list)
+    # runRandomForest(case_data, case_target, 1)
+    kernel_value = 'sigmoid'
+    c_value = 0.01
+    clf = SVC(kernel=kernel_value, C=c_value, cache_size=1000)
+    numeric_case_target = []
+    for t in case_target:
+        if t == 'sim':
+            numeric_case_target.append(float(1))
+        else:
+            numeric_case_target.append(float(0))
+    y_pred = cross_val_predict(clf, case_data, case_target, cv=10)
+    conf_mat = confusion_matrix(case_target, y_pred)
+    others = precision_recall_fscore_support(case_target, y_pred)
+    print(conf_mat)
+    print(others)
 
